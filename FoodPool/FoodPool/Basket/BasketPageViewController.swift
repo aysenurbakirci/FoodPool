9f70f9812a86857b258245e3d9ac77274c1cb969
Ayşe Nur Bakırcı <ayse.bakirci@yemeksepeti.com>
2022-03-10 22:45:56 +0300

    Add the ability to order from more than one restaurant

--- FoodPool/FoodPool/Basket/BasketPageViewController.swift
+++ FoodPool/FoodPool/Basket/BasketPageViewController.swift
@@ -27,48 +27,37 @@ extension BasketPageViewController: UITableViewDelegate {}
 extension BasketPageViewController: UITableViewDataSource {
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return viewModel.numberOfSections()
+        return 3
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return viewModel.numberOfRowsInSection(for: section)
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        guard let view = tableView.dequeueReusableHeaderFooterView(withIdentifier: HeaderView.reuseIdentifier) as? HeaderView else {
+            return nil
+        }
+        return view
     }
     
     func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
-        let basketModel = viewModel.modelForSection(in: section)
-        switch basketModel {
-        case .restaurant(_):
-            return nil
-        case .mealList(_):
-            guard let view = tableView.dequeueReusableHeaderFooterView(withIdentifier: FooterView.reuseIdentifier) as? FooterView else {
-                return nil
-            }
-            return view
+        guard section == 2 else { return UIView() }
+        guard let view = tableView.dequeueReusableHeaderFooterView(withIdentifier: FooterView.reuseIdentifier) as? FooterView else {
+            return UIView()
         }
+        return view
+
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let basketModel = viewModel.modelForSection(in: indexPath.section)
-        switch basketModel {
-        case .restaurant(let string):
-            let cell: RestaurantCell = tableView.dequeue(for: indexPath)
-            cell.apply(imageName: "image", title: string, description: "Description")
-            cell.selectionStyle = .none
-            cell.separatorInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: .greatestFiniteMagnitude)
-            cell.directionalLayoutMargins = .zero
-            return cell
-        case .mealList(let mealList):
-            let cell: BasketCell = tableView.dequeue(for: indexPath)
-            let model = mealList[indexPath.row]
-            cell.apply(title: model.mealName, price: model.price, count: model.count)
-            cell.addTarget(self)
-            return cell
-        }
+        let cell: BasketCell = tableView.dequeue(for: indexPath)
+        return cell
     }
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            viewModel.deleteItem(at: indexPath)
+//            viewModel.deleteItem(at: indexPath)
             tableView.deleteRows(at: [indexPath], with: .fade)
         }
     }
---
+++ FoodPool/FoodPool/Basket/Cell/HeaderView.swift
@@ -0,0 +1,54 @@
+//
+//  HeaderView.swift
+//  FoodPool
+//
+//  Created by Ayşe Nur Bakırcı on 10.03.2022.
+//
+
+import Foundation
+import UIKit
+import LayoutKit
+import AutoLayoutHelper
+import FoodPoolKit
+
+final class HeaderView: UITableViewHeaderFooterView {
+    
+    static let reuseIdentifier = "HeaderView"
+    
+    lazy var restaurantImage: UIImageView = .create(image: .photo)
+    
+    lazy var title: UILabel = .create(
+        lblText: "Restaurant Name",
+        font: .boldLarge,
+        textColor: .title,
+        numberOfLines: 0,
+        textAlignment: .left,
+        sizeToFit: true
+    )
+    
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+        
+        contentView.addSubviews(restaurantImage, title)
+        contentView.backgroundColor = .white
+        contentView.addCornerRadius(radius: 20.0)
+        
+        restaurantImage.anchor(top: contentView.topAnchor,
+                               leading: contentView.leadingAnchor,
+                               bottom: contentView.bottomAnchor,
+                               padding: .equalPadding(10.0))
+        restaurantImage.sizeAnchor(height: 60.0)
+        restaurantImage.aspect(ratio: 1)
+        
+        title.anchor(top: contentView.topAnchor,
+                     leading: restaurantImage.trailingAnchor,
+                     bottom: contentView.bottomAnchor,
+                     trailing: contentView.trailingAnchor,
+                     padding: .equalPadding(10.0))
+
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
