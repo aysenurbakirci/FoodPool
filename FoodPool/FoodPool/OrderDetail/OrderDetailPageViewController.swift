1f83f5d93c24a724ca90272db3f67b3f7f183fa1
Ayşe Nur Bakırcı <ayse.bakirci@yemeksepeti.com>
2022-03-15 11:28:14 +0300

    Fix order detail page animation

--- FoodPool/FoodPool/OrderDetail/OrderDetailPageViewController.swift
+++ FoodPool/FoodPool/OrderDetail/OrderDetailPageViewController.swift
@@ -8,42 +8,46 @@
 import Foundation
 import UIKit
 
-final class OrderDetailPageViewController: UIViewController {
+class OrderDetailPageViewController: UIViewController {
     
     let shapeLayer = CAShapeLayer()
     let trackLayer = CAShapeLayer()
-    
+    let lineWidth: CGFloat = 10.0
+    let color: UIColor = .black
+    let duration: TimeInterval = 2
+    let endValue = 0.7
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .background
-
-        let circularPath = UIBezierPath(arcCenter: view.center, radius: 120, startAngle: .pi / -2, endAngle: .pi * 2, clockwise: true)
+        
+        let center = view.center
+        let circularPath = UIBezierPath(arcCenter: center, radius: 100, startAngle: .pi / -2, endAngle: .pi * 2, clockwise: true)
         
         trackLayer.path = circularPath.cgPath
-        trackLayer.strokeColor = UIColor.secondaryColor.cgColor
-        trackLayer.lineWidth = 16
+        trackLayer.strokeColor = color.withAlphaComponent(0.2).cgColor
+        trackLayer.lineWidth = lineWidth
         trackLayer.lineCap = .round
         trackLayer.fillColor = UIColor.clear.cgColor
         view.layer.addSublayer(trackLayer)
         
-        trackLayer.path = circularPath.cgPath
-        trackLayer.strokeColor = UIColor.mainColor.cgColor
-        trackLayer.lineWidth = 16
-        trackLayer.lineCap = .round
-        trackLayer.fillColor = UIColor.clear.cgColor
+        shapeLayer.path = circularPath.cgPath
+        shapeLayer.strokeColor = color.cgColor
+        shapeLayer.lineWidth = lineWidth
+        shapeLayer.lineCap = .round
+        shapeLayer.fillColor = UIColor.clear.cgColor
         shapeLayer.strokeEnd = 0
-        view.layer.addSublayer(trackLayer)
-        
-        view.addGestureRecognizer(UIGestureRecognizer(target: self, action: #selector(tapHandle)))
+        view.layer.addSublayer(shapeLayer)
+
     }
     
-    @objc func tapHandle() {
-        print("click")
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         let basicAnimation = CABasicAnimation(keyPath: "strokeEnd")
-        basicAnimation.toValue = 0.7
-        basicAnimation.duration = 2
+        basicAnimation.toValue = endValue
+        basicAnimation.duration = duration
         basicAnimation.fillMode = .forwards
         basicAnimation.isRemovedOnCompletion = false
         shapeLayer.add(basicAnimation, forKey: "basicAnimation")
     }
+    
 }
